df15b4142c655f17a329f0ed2919421145315572
Jose Pablo Brenes Alfaro <josepablo1997@hotmail.com>
2017-09-01 22:17:04 -0600

    Others
    ...

--- TreasureHunter/src/pk/codeapp/methods/Methods.java
+++ TreasureHunter/src/pk/codeapp/methods/Methods.java
@@ -5,6 +5,12 @@
  */
 package pk.codeapp.methods;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import pk.codeapp.model.User;
 
 /**
@@ -14,12 +20,14 @@ import pk.codeapp.model.User;
 public class Methods {
     private User root,end;
     private User newUser;
+   
     
     public boolean add_User(String name,String userName,String email,String password,String rol){ //Cheack border sig
         if(isValidEmailAddress(email)==true){
             newUser=new User(name, userName, email, password, rol);
             if(root==null){
                 root =end= newUser;
+                writeUser(newUser); // add to BinaryFile
                 return true;
             }else{
                 newUser.setSig(end);
@@ -27,17 +35,63 @@ public class Methods {
                 root.setSig(newUser);
                 newUser.setAnt(root);
                 end=newUser;
+                writeUser(newUser);
                 return true;
             }
         }
         return false;
     }
-    
-    
     public boolean isValidEmailAddress(String email) {
            String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$"; // Email Pattern 
            java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern); // Compile Email
            java.util.regex.Matcher m = p.matcher(email); // Check for that same pattern
            return m.matches();
     }
+    public  void writeUser(User newUser) // Methods to save users in binaryfile
+    {
+        String namefile = "/pk.codeapp.tools/users.dat";
+        try 
+        {
+            FileOutputStream outFile = new FileOutputStream(namefile);
+            ObjectOutputStream objectOut = new ObjectOutputStream(outFile);
+            // se escriben dos objetos de la clase Persona
+            objectOut.writeObject(newUser);
+            objectOut.close();
+            System.out.println("Se ha agregado con exito");
+        }
+        catch (FileNotFoundException e) 
+        {
+            System.out.println("¡El fichero no existe!");
+        } catch (IOException e) 
+        {
+            System.out.println(e.getMessage());
+        } catch (Exception e) 
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+    public User readUser(){
+        String namefile = "/pk.codeapp.tools/users.dat";
+        try {
+            FileInputStream ficheroEntrada = new FileInputStream(namefile);
+            ObjectInputStream objetoEntrada = new ObjectInputStream(ficheroEntrada);
+            User User;
+            // se leen dos objetos de la clase Persona
+            return User readUser = (User)objetoEntrada.readObject();
+            // se cierra el flujo de objetos objetoEntrada
+            objetoEntrada.close();
+            System.out.println("DNI\t Nombre");
+            System.out.println(p1);
+            System.out.println(p2);
+            } catch (FileNotFoundException e) {
+            System.out.println("¡El fichero no existe!");
+            } catch (IOException e) {
+            System.out.println(e.getMessage());
+            } catch (Exception e) {
+            System.out.println(e.getMessage());
+            };
+        return null;
+    }
+    
+    
 }
