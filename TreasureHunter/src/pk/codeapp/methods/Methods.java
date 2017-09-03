4d7586bab4093ff8edfae11c8d66f041e7e5bc02
jdamador <amadorsalasjd@gmail.com>
2017-09-02 20:32:36 -0600

    Update Methods
    change the methods for read users!!!

--- TreasureHunter/src/pk/codeapp/methods/Methods.java
+++ TreasureHunter/src/pk/codeapp/methods/Methods.java
@@ -17,12 +17,14 @@ import pk.codeapp.model.User;
  *
  * @author Jose Pablo Brenes
  */
-public class Methods {
+public class Methods
+{
 
     private User root, end;
     private User newUser;
 
-    public boolean add_User(String name, String userName, String email, String password, String rol) { //Cheack border sig
+    public boolean add_User(String name, String userName, String email, String password, String rol)
+    { //Cheack border sig
         if (isValidEmailAddress(email) == true) {
             newUser = new User(name, userName, email, password, rol);
             if (root == null) {
@@ -44,7 +46,8 @@ public class Methods {
         return false;
     }
 
-    public boolean isValidEmailAddress(String email) {
+    public boolean isValidEmailAddress(String email)
+    {
         String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$"; // Email Pattern 
         java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern); // Compile Email
         java.util.regex.Matcher m = p.matcher(email); // Check for that same pattern
@@ -57,8 +60,9 @@ public class Methods {
         try {
             FileOutputStream outFile = new FileOutputStream(namefile);
             ObjectOutputStream objectOut = new ObjectOutputStream(outFile);
-            if (readUser(newUser.getNameUser()) == null) {
+            if (readUser(newUser.getUserName()) == null) {
                 objectOut.writeObject(newUser);
+                objectOut.writeObject(null);
                 objectOut.close();
                 System.out.println("Se ha agregado con exito");
             } else {
@@ -70,26 +74,28 @@ public class Methods {
             System.out.println("entro1");
             System.out.println(e.getMessage());
         } catch (Exception e) {
-             System.out.println("entro2");
+            System.out.println("entro2");
             System.out.println(e.getMessage());
         }
     }
 
-    public User readUser(String userName) { // Methods to read users in binaryfile and return object user
+    public User readUser(String userName)
+    { // Methods to read users in binaryfile and return object user
         String namefile = "users.dat";
 
         try {
             FileInputStream inFile = new FileInputStream(namefile);
             ObjectInputStream inObject = new ObjectInputStream(inFile);
-
             User readUser = (User) inObject.readObject();
+            
             while (readUser != null) {
-                if (readUser.getUserName() == userName) {
-                    System.out.println("Nombre de usuario es ");
-                   inObject.close();
+                System.out.println("Estuve aqui!");
+                if (readUser.getUserName().equalsIgnoreCase(userName)) {
+                    inObject.close();
                     return readUser;
                 } else {
                     readUser = (User) inObject.readObject();
+                   
                 }
             }
             inObject.close();
@@ -100,7 +106,7 @@ public class Methods {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         };
-       
+
         return null;
     }
 
